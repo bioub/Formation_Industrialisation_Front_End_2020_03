ab4816d05bb905f13f27121a69c177904fa88d90
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-03-11 12:42:59 +0100

    chore: correction webpack

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,6 +1,8 @@
+import config from './config.json5';
+
 function hello(name) {
   //return 'Hello ' + name + '!!!';
-  return `Hello ${name} !!!`;
+  return `${config.message} ${name} !!!`;
 }
 
 export { hello };
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,56 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BannerPlugin } = require('webpack');
 
-/** @type {import('webpack').Configuration} */
-const config = {
-  devtool: 'source-map',
-  entry: './src/js/main.js',
-  plugins: [
+function configFactory(_, { mode }) {
+  const plugins = [
+    new BannerPlugin(`Copyright ${new Date().getFullYear()} Orsys`),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-    ],
-  },
-};
+  ];
 
-module.exports = config;
+  if (mode === 'production') {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'app.[hash].min.css',
+      }),
+    );
+  }
+
+  /** @type {import('webpack').Configuration} */
+  const config = {
+    devtool: mode !== 'production' ? 'source-map' : false,
+    entry: './src/js/main.js',
+    output: {
+      filename: mode !== 'production' ? 'app.js' : 'app.[hash].min.js',
+    },
+    plugins,
+    module: {
+      rules: [
+        {
+          test: /\.json5$/,
+          use: 'json5-loader',
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+  };
+
+  return config;
+}
+
+module.exports = configFactory;
 
 // Exercice 1
 // Utiliser json5-loader pour permettre de charger des fichiers
@@ -34,11 +58,14 @@ module.exports = config;
 // https://webpack.js.org/loaders/json5-loader/
 
 // Exercice 2
-// Utiliser banner-plugin pour afficher dans une "bannière" un copyright "Copyright 2020 Nous"
+// Utiliser banner-plugin pour afficher dans une "bannière" un
+// copyright "Copyright 2020 Nous"
 // en haut de chaque fichier buildé
-// https://webpack.js.org/plugins/banner-plugin/
+// https://webpack.js.org/plugins/banner-plugin/$
+// tester avec : npm run build:dev
 
 // Exercice 3
 // Utiliser mini-css-extract-plugin pour extraire les fichiers css
 // dans un fichier externe et les charger via une balise link
 // https://webpack.js.org/plugins/mini-css-extract-plugin/
+// tester avec : npm run build:dev
