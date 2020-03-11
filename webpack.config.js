d6a365aec95398502b97075956f503f474225d80
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-03-11 11:03:06 +0100

    chore: exercice webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,3 +27,18 @@ const config = {
 };
 
 module.exports = config;
+
+// Exercice 1
+// Utiliser json5-loader pour permettre de charger des fichiers
+// au format .json5
+// https://webpack.js.org/loaders/json5-loader/
+
+// Exercice 2
+// Utiliser banner-plugin pour afficher dans une "bannière" un copyright "Copyright 2020 Nous"
+// en haut de chaque fichier buildé
+// https://webpack.js.org/plugins/banner-plugin/
+
+// Exercice 3
+// Utiliser mini-css-extract-plugin pour extraire les fichiers css
+// dans un fichier externe et les charger via une balise link
+// https://webpack.js.org/plugins/mini-css-extract-plugin/
