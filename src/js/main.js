e0f5800f77e45c72b63efda2bd01c75b6e7ae2f4
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-03-11 10:43:25 +0100

    chore: build html, scss, bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
+import '../scss/app.scss';
+import 'bootstrap';
+
 /** @type {HTMLInputElement} */
 import { hello } from './hello.js';
+// const { hello } = require('./hello.js');
 
 const inputPrenomEl = document.querySelector('.prenom');
 /** @type {HTMLParagraphElement} */
